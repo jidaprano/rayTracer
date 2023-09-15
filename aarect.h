a5f4d0ae0940ae640144d17d328eee85316bbba8
jidaprano <117032914+jidaprano@users.noreply.github.com>
2023-09-15 15:09:54 -0400

    Add files via upload

--- aarect.h
+++ aarect.h
@@ -3,7 +3,17 @@
 
 #include "rtweekend.h"
 #include "hittable.h"
+#include <vector>
 
+//Vertex struct
+struct Vertex {
+    Vertex() {}
+    Vertex(vec3 pos, vec3 norm) : position(pos), normal(norm) {}
+    vec3 position;
+    vec3 normal;
+};
+
+//Rect classes and hit functions
 class xy_rect : public hittable {
     public:
         double x0, x1, y0, y1, k;
@@ -19,9 +29,10 @@ class xy_rect : public hittable {
         virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
 
         virtual bool bounding_box(double time0, double time1, aabb& output_box) const override {
-            // The bounding box must have non-zero width in each dimension, so pad the Z
+            
+            // The bounding box must have non-zero width in each dimension, so pad the X
             // dimension a small amount.
-            output_box = aabb(point3(x0,y0, k-0.0001), point3(x1, y1, k+0.0001));
+            output_box = aabb(point3(x0, y0, k-0.0001), point3(x1, y1, k+0.0001));
             return true;
         }
 };
@@ -123,4 +134,72 @@ bool yz_rect::hit(const ray& r, double t_min, double t_max, hit_record& rec) con
     return true;
 }
 
-#endif
+// //Triangle classes and hit functions
+// class triangle : public hittable {
+//     public:
+//         triangle(int a, int b, int c, std::vector<Vertex>* vertices, material* mat);
+//         virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
+        
+//         bool triangle::bounding_box(double time0, double time1, aabb& output_box) const {
+//                 // The bounding box must have non-zero width in each dimension, so pad the Y
+//                 // dimension a small amount.
+//                 output_box = aabb(point3(x0,k-0.0001,z0), point3(x1, k+0.0001, z1));
+//                 return true;
+//             }
+    
+//     private:
+//         std::vector<Vertex>* vertices;
+//         int a, b, c;
+//         std::shared_ptr<material> mat_ptr;
+// };
+
+// bool triangle::hit(const ray& r, float t_min, float t_max, hit_record& rec)const
+//     {
+//         const float EPSILON = 0.0000001;
+ 
+//         vec3 edge1, edge2, h, s, q, ca, cb, cc;
+//         float a, f, u, v;
+ 
+//         ca = vertices->at(a).position;
+//         cb = vertices->at(b).position;
+//         cc = vertices->at(c).position;
+ 
+//         edge1 = cb - ca;
+//         edge2 = cc - ca;
+         
+//         h = cross(r.direction(), edge2);
+//         a = dot(edge1, h);
+//         if (a > -EPSILON && a < EPSILON)
+//             return false;    // This ray is parallel to this triangle.
+ 
+//         f = 1.0 / a;
+//         s = r.origin() - ca;
+//         u = f * dot(s, h);
+//         if (u < 0.0 || u > 1.0)
+//             return false;
+ 
+//         q = cross(s, edge1);
+//         v = f * dot(r.direction(), q);
+//         if (v < 0.0 || u + v > 1.0)
+//             return false;
+ 
+//         // At this stage we can compute t to find out where the intersection point is on the line.
+//         float t = f * dot(edge2, q);
+//         if (t > EPSILON && t < 1 / EPSILON) // ray intersection
+//         {
+//             if (t < t_max && t > t_min) //object closer than previous
+//             {
+//                 rec.t = t;
+//                 rec.p = r.origin() + r.direction() * t;
+//                 rec.normal = h;
+//                 rec.mat_ptr = mat_ptr;
+//                 return true;
+//             }
+//             else
+//                 return false; //this means there is a ray intersection, but it is behind a differend object
+//         }
+//         else // This means that there is a line intersection but not a ray intersection.
+//             return false;
+//     }
+
+ #endif
